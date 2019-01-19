8a61a7319c8b83febdb92abbb52eb9677ca52aff
iamwangyabin <yabinwango@gmail.com>
2019-01-19 19:11:09 +0800

    ComputeCorrespondenceByEigenVec

--- src/GraphMatching.cpp
+++ src/GraphMatching.cpp
@@ -60,6 +60,7 @@ GraphMatching::GraphMatching(
         }
     }
 }
+
 bool GraphMatching::judge_pointn_plane(std::vector<pcl::PointXYZ>& arr)
 {
     Eigen::Vector3f v1, v2, v3;
@@ -88,3 +89,168 @@ bool GraphMatching::judge_pointn_plane(std::vector<pcl::PointXYZ>& arr)
     }
     return true;
 }
+
+pcl::CorrespondencesPtr GraphMatching::ComputeCorrespondenceByEigenVec(int best_num /* = 4 */)
+{
+    pcl::CorrespondencesPtr cor4(new pcl::Correspondences);
+    std::vector<int> result4;
+    // 特征值和特征向量
+    arma::fvec eigval;
+    arma::fmat eigvec;
+    // Obtain a limited number of eigenvalues and eigenvectors of sparse symmetric real matrix X
+    arma::eigs_sym(eigval, eigvec, _m, 1, "la");
+
+    if (eigvec.size() <= 0)
+        return cor4;
+
+    result4.resize(eigvec.size());
+    for (int i = 0; i < result4.size(); ++i)
+        result4[i] = 0;
+
+    // 取得绝对值
+    for (int i = 0; i < eigvec.size(); i++)
+        if (eigvec(i) < 0)
+            eigvec(i) = -eigvec(i);
+
+
+    int num = 0;
+    arma::fmat::iterator biggest = std::max_element(eigvec.begin(), eigvec.end());
+    while (*biggest > 0.001)
+    {
+        // only use the best num correspondences
+        if (num >= best_num)
+            break;
+
+        int index = std::distance(eigvec.begin(), biggest);
+        result4[index] = 1;
+        int i = index / _keypoints2.size();
+        int j = index % _keypoints2.size();
+
+        int start = i*_keypoints2.size();
+        for (int inx = 0; inx < _keypoints2.size(); inx += 1)
+            eigvec(start + inx) = 0.0;
+
+        for (int inx = j; inx < _keypoints1.size()*_keypoints2.size(); inx += _keypoints2.size())
+            eigvec(inx) = 0.0;
+
+        biggest = std::max_element(eigvec.begin(), eigvec.end());
+        num++;
+    }
+
+    // convert to pair
+    if (num < best_num)
+    {
+        return cor4;
+    }
+    for (int i = 0; i < result4.size(); ++i)
+    {
+        if (result4[i] == 1)
+        {
+            pcl::Correspondence c;
+            c.index_query = i / _keypoints2.size();
+            c.index_match = i % _keypoints2.size();
+            cor4->push_back(c);
+        }
+    }
+
+    bool valid = true;
+    for (int i = 0; i < cor4->size(); i++)
+    {
+        int frame1_query1 = cor4->at(i).index_query;
+        int frame2_match1 = cor4->at(i).index_match;
+
+        for (int j = i + 1; j < cor4->size(); ++j)
+        {
+            int frame1_query2 = cor4->at(j).index_query;
+            int frame2_match2 = cor4->at(j).index_match;
+
+            if (frame1_query1 == frame1_query2 || frame2_match1 == frame2_match2)
+            {
+                valid = false;
+                break;
+            }
+
+            float dis_model1_x = _keypoints1.points[frame1_query1].x - _keypoints1.points[frame1_query2].x;
+            float dis_model1_y = _keypoints1.points[frame1_query1].y - _keypoints1.points[frame1_query2].y;
+            float dis_model1_z = _keypoints1.points[frame1_query1].z - _keypoints1.points[frame1_query2].z;
+            if (fabs(dis_model1_x) < 0.01 && fabs(dis_model1_y) < 0.01 && fabs(dis_model1_z) < 0.01)
+            {
+                valid = false;
+                break;
+            }
+
+            float dis_model2_x = _keypoints2.points[frame2_match1].x - _keypoints2.points[frame2_match2].x;
+            float dis_model2_y = _keypoints2.points[frame2_match1].y - _keypoints2.points[frame2_match2].y;
+            float dis_model2_z = _keypoints2.points[frame2_match1].z - _keypoints2.points[frame2_match2].z;
+            if (fabs(dis_model2_x) < 0.01 && fabs(dis_model2_y) < 0.01 && fabs(dis_model2_z) < 0.01)
+            {
+                valid = false;
+                break;
+            }
+        }
+    }
+    if (!valid)
+    {
+        cor4->clear();
+        return cor4;
+    }
+
+
+    // judge 4 points in the same plane
+    std::vector<pcl::PointXYZ> arr1;
+    std::vector<pcl::PointXYZ> arr2;
+    pcl::PointXYZ p[4];
+    for (int i = 0; i < 4; ++i)
+    {
+        int frame1_query = cor4->at(i).index_query;
+        int frame2_match = cor4->at(i).index_match;
+
+        pcl::PointXYZ p1, p2;
+        p1.x = _keypoints1.points[frame1_query].x;	p1.y = _keypoints1.points[frame1_query].y;	p1.z = _keypoints1.points[frame1_query].z;
+        p2.x = _keypoints2.points[frame2_match].x;	p2.y = _keypoints2.points[frame2_match].y;	p2.z = _keypoints2.points[frame2_match].z;
+        arr1.push_back(p1);
+        arr2.push_back(p2);
+    }
+    int add_num = 0;
+    while ((judge_pointn_plane(arr1) || judge_pointn_plane(arr2)) && add_num < 16 && *biggest > 0.001)
+    {
+        // add new edge
+        int index = std::distance(eigvec.begin(), biggest);
+        result4[index] = 1;
+        int i = index / _keypoints2.size();
+        int j = index % _keypoints2.size();
+
+        int start = i*_keypoints2.size();
+        for (int inx = 0; inx < _keypoints2.size(); inx += 1)
+            eigvec(start + inx) = 0.0;
+
+        for (int inx = j; inx < _keypoints1.size()*_keypoints2.size(); inx += _keypoints2.size())
+            eigvec(inx) = 0.0;
+
+        // convert to pair
+        pcl::Correspondence c;
+        c.index_query = index / _keypoints2.size();
+        c.index_match = index % _keypoints2.size();
+        cor4->push_back(c);
+
+        int frame1_query = index / _keypoints2.size();
+        int frame2_match = index % _keypoints2.size();
+
+        pcl::PointXYZ p1, p2;
+        p1.x = _keypoints1.points[frame1_query].x;	p1.y = _keypoints1.points[frame1_query].y;	p1.z = _keypoints1.points[frame1_query].z;
+        p2.x = _keypoints2.points[frame2_match].x;	p2.y = _keypoints2.points[frame2_match].y;	p2.z = _keypoints2.points[frame2_match].z;
+        arr1.push_back(p1);
+        arr2.push_back(p2);
+
+        biggest = std::max_element(eigvec.begin(), eigvec.end());
+        add_num++;
+    }
+
+
+    // transformation keypoints1 to keypoints 2
+    Eigen::Matrix4f	transformation2 = ComputeRigid(cor4, _keypoints1, _keypoints2);
+
+    _transformation = transformation2;
+    return cor4;
+}
+
